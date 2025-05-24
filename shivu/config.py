21633e1614f4b3bc8784aa59ffb3e93348446a71
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-05-24 10:01:49 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,12 +5,12 @@ class Config(object):
     OWNER_ID = "6765826972"
     sudo_users = "6845325416", "6765826972"
     GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    TOKEN = "7507037404:AAHQkZN9UPmIWGIR48QnOI2pn4IF8YsuZI8"
+    mongo_url = "mongodb+srv://worker:TFqF209jhTbnWDAN@cluster0.if6ahq2.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_em_support"
     UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
+    BOT_USERNAME = "sincatcherbot"
     CHARA_CHANNEL_ID = "-1002133191051"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
