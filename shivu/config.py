c5e8e8670d83e8e4bdcf53612376dd98671638b6
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-05-24 12:58:41 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,14 +4,14 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6765826972"
     sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
+    GROUP_ID = -1002501304890
     TOKEN = "7507037404:AAHQkZN9UPmIWGIR48QnOI2pn4IF8YsuZI8"
     mongo_url = "mongodb+srv://worker:TFqF209jhTbnWDAN@cluster0.if6ahq2.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "+9y7_hAj6wpMyZGZl"
     UPDATE_CHAT = "Collect_em_support"
     BOT_USERNAME = "sincatcherbot"
-    CHARA_CHANNEL_ID = "-1002133191051"
+    CHARA_CHANNEL_ID = ""
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
