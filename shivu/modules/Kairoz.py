9977fde5dc54972e48aee3282c82dba4dca8ff43
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-05-26 19:18:25 +0530

    Update Kairoz.py

--- shivu/modules/Kairoz.py
+++ shivu/modules/Kairoz.py
@@ -15,14 +15,14 @@ async def kairoz_cmd(_, message: Message):
     if user_data is None:
         await create_kairoz_user(user_id)
         await message.reply_text(
-            "*_You're not initialized yet. Please use /start to begin._*",
+            "***You're not initialized yet. Please use /start to begin.***",
             parse_mode=ParseMode.MARKDOWN
         )
         return
 
     balance = await get_user_kairoz_balance(user_id)
     await message.reply_text(
-        f"*_*Current [K](https://i.ibb.co/ymvNjsTs/tmpyx38ufcs.jpg)airoz Balance:*_* `{KAIROZ_SYMBOL}{balance:,}`",
+        f"***Current [K](https://i.ibb.co/ymvNjsTs/tmpyx38ufcs.jpg)airoz Balance:*** `{KAIROZ_SYMBOL}{balance:,}`",
         parse_mode=ParseMode.MARKDOWN,
         disable_web_page_preview=False
     )
