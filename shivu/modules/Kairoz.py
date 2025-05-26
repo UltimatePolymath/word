fc29c596aa1e4deff06e9dc8be53b6d28ae3b212
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-05-26 19:07:30 +0530

    Create Kairoz.py

---
+++ shivu/modules/Kairoz.py
@@ -0,0 +1,28 @@
+from pyrogram import filters
+from pyrogram.enums import ParseMode
+from pyrogram.types import Message
+
+from shivu import shivuu
+from shivu.archive.kairoz import get_user_kairoz_balance, create_kairoz_user
+
+KAIROZ_SYMBOL = "₭"
+
+@shivuu.on_message(filters.command("kairoz"))
+async def kairoz_cmd(_, message: Message):
+    user_id = message.from_user.id
+
+    user_data = await get_user_kairoz_balance(user_id)
+    if user_data is None:
+        await create_kairoz_user(user_id)
+        await message.reply_text(
+            "*_You're not initialized yet. Please use /start to begin._*",
+            parse_mode=ParseMode.MARKDOWN
+        )
+        return
+
+    balance = await get_user_kairoz_balance(user_id)
+    await message.reply_text(
+        f"*_*Current [K](https://i.ibb.co/ymvNjsTs/tmpyx38ufcs.jpg)airoz Balance:*_* `{KAIROZ_SYMBOL}{balance:,}`",
+        parse_mode=ParseMode.MARKDOWN,
+        disable_web_page_preview=False
+    )
