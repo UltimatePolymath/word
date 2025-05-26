b7cd3c76b32d974e134981fa7e97836690600ca5
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-05-26 13:50:37 +0530

    Update __init__.py

--- shivu/sin/__init__.py
+++ shivu/sin/__init__.py
@@ -15,10 +15,7 @@ LOGGER = logging.getLogger(__name__)
 # =========================
 # Currency MongoDB Setup
 # =========================
-CURRENCY_MONGO_URL = (
-    "mongodb+srv://worker:TFqF209jhTbnWDAN@cluster0.if6ahq2.mongodb.net/"
-    "?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
-)
+CURRENCY_MONGO_URL = "mongodb+srv://worker:TFqF209jhTbnWDAN@cluster0.if6ahq2.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
 
 currency_client = AsyncIOMotorClient(CURRENCY_MONGO_URL)
 currency_db = currency_client["currency_database"]
